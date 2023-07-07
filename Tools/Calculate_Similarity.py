57ab11759f003bca167d02ac9e8e6ac4c568a88d
Kruel, Agustin <agustin.kruel@pnnl.gov>
2023-07-07 21:40:42 +0000

    Add sanitization helper function

--- Tools/Calculate_Similarity.py
+++ Tools/Calculate_Similarity.py
@@ -1,7 +1,32 @@
 from rdkit.Chem import MolFromSmiles, AllChem, MACCSkeys, RDKFingerprint
 from scipy.spatial import distance
 
-## This should be the default
+## Helper function to fix some SMILES inconsistencies instead of always spitting it out:
+
+from rdkit.Chem import SanitizeMol, SanitizeFlags
+def partially_sanitize(smi):
+    """
+    From the RDKit Documentation: https://www.rdkit.org/docs/Cookbook.html#explicit-valence-error-partial-sanitization
+
+    Mostly just makes sure the charge of any hypervalent atoms is explicit for RDKit. (In my experience, very common in Nitrogen, Oxygen, and Sulfur)
+    I usually need this when working with Fingerprints (they are very strict)
+
+    I'm noticing some weird behavior with ignoring SMILES' implicit hydrogens but I need to test more. 
+    For now, just using MolFromSmiles() and no OPENBABEL or manually remove implicit hypervalence is fine
+    e.g. n implicit isonitriles, nitrates, or quaternary amines (CN#C -> C[N+]#[C-])
+
+    I just think this is necessary for the final tool to ingest *any* smiles or slightly wrong ones (see: LLM)
+    ESPECIALLY if we're also using RDKit or ML to generate new molecule strings
+    """
+    if '\t' in smi: smi = smi.split('\t')[0] # weird OPENBABEL syntax
+    mol = MolFromSmiles(smi,sanitize=False) # if True, this is where RDKit throws error. False=skip valence calc
+    mol.UpdatePropertyCache(strict=False)  # recalculate the valences, be lenient
+    # then, redo the molecule with charges, aromaticity, etc explicitly stated
+    SanitizeMol(mol,SanitizeFlags.SANITIZE_FINDRADICALS|SanitizeFlags.SANITIZE_KEKULIZE|SanitizeFlags.SANITIZE_SETAROMATICITY|SanitizeFlags.SANITIZE_SETCONJUGATION|SanitizeFlags.SANITIZE_SETHYBRIDIZATION|SanitizeFlags.SANITIZE_SYMMRINGS,catchErrors=True)
+    return mol
+
+## This should be the default:
+
 def calculate_similarity(compound_1: str, compound_2: str) -> float:
     """
     Calculate (Jaccard-Tanimoto) similarity value between exactly 2 molecules (Morgan).

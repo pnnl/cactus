d742719dc262aeac10fa4554e53bc759cf417830
mcna892 <andrew.mcnaughton@pnnl.gov>
2023-10-09 23:12:08 -0700

    Fixing merge conflicts

--- src/cactus/agent/cactus.py
+++ src/cactus/agent/cactus.py
@@ -72,8 +72,6 @@ class Cactus:  # pylint: disable=too-few-public-methods
             print(f"Module name: {model_name}, not found!")
 
         if tools is None:
-<<<<<<< HEAD
-<<<<<<< HEAD
             tools_llm = _load_model(model_name, cache_dir, max_length, use_8bit)
             tools = make_tools(llm, verbose=True)
             self.agent_executor = AgentExecutor.from_agent_and_tools(
@@ -88,51 +86,3 @@ class Cactus:  # pylint: disable=too-few-public-methods
     def run(self, prompt):
         outputs = self.agent_executor({"input": prompt})
         return outputs["output"]
-=======
-            tools_llm=_load_model(
-                model_name,
-                cache_dir,
-                max_length, 
-                use_8bit )
-            tools=make_tools(
-                llm,
-                verbose=verbose
-            )    
-            self.agent_executor = AgentExecutor.from_agent_and_tools(
-            tools=tools,
-            agent=ZeroShotAgent.from_llm_and_tools(
-                llm,
-                tools,
-            ),
-            verbose=True
-            )
-
-
-    def run(self, prompt):
-        outputs = self.agent_executor({"input": prompt})
-=======
-            tools_llm=_load_model(
-                model_name,
-                cache_dir,
-                max_length, 
-                use_8bit )
-            tools=make_tools(
-                llm,
-                verbose=verbose
-            )    
-            self.agent_executor = AgentExecutor.from_agent_and_tools(
-            tools=tools,
-            agent=ZeroShotAgent.from_llm_and_tools(
-                llm,
-                tools,
-            ),
-            verbose=True
-            )
-
-
-    def run(self, prompt):
-        outputs = self.agent_executor({"input": prompt})
->>>>>>> main
-        return outputs['output']
-        pass
->>>>>>> 507babc (agent changes)

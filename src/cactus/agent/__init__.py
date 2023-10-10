76df47e895e2247caf6a81114d65ca790e8be161
mcna892 <andrew.mcnaughton@pnnl.gov>
2023-10-09 23:12:08 -0700

    Fixing merge conflicts

--- src/cactus/agent/__init__.py
+++ src/cactus/agent/__init__.py
@@ -1,14 +1,4 @@
 from .cactus import Cactus
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .tools import make_tools
 
 __all__ = ["Cactus", "make_tools"]
-=======
-
-__all__ = ["Cactus"]
->>>>>>> 95cc70a (refactoring to src/cactus)
-=======
-
-__all__ = ["Cactus"]
->>>>>>> main
--- src/cactus/agent/tools.py
+++ src/cactus/agent/tools.py
@@ -1,7 +1,6 @@
 import os
 from langchain import agents
 from langchain.base_language import BaseLanguageModel
-<<<<<<< HEAD
 from cactus.tools import (
     cas_to_SMILES,
     chemblid_to_SMILES,
@@ -31,24 +30,6 @@ def make_tools(
     )
 
     all_tools += [
-=======
-from cactus.tools import *
-
-def make_tools(
-    llm : BaseLanguageModel
-    #api_keys: dict={}
-    verbose=True
-):
-    all_tools=agents.load_tools([
-        "python_repl",
-        "ddg-search",
-        "wikipedia",
-        "Calculator",
-        
-    ])
-    
-    all_tools+=[
->>>>>>> main
         inchikey_to_SMILES(),
         name_to_SMILES(),
         cas_to_SMILES(),
@@ -58,7 +39,6 @@ def make_tools(
         zinc_id_to_SMILES(),
         calculate_MolWt(),
         calculate_QED(),
-<<<<<<< HEAD
         #        brenk_filter(),
         calculate_TPSA(),
         #        calculate_bbb_permeant(),
@@ -70,18 +50,3 @@ def make_tools(
     ]
 
     return all_tools
-=======
-        brenk_filter(),
-        calculate_TPSA(),
-        calculate_bbb_permeant(),
-        calculate_druglikeness(),
-        calculate_gi_absorption(),
-        calculate_logp(),
-        pains_filter(),
-        calculate_SA()
-        
-    ]
-    
-    return all_tools
-    
->>>>>>> main

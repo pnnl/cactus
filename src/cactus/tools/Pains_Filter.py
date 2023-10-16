5f827e0c1482b0ecbc126d3143734bd4340c0ccf
mcna892 <andrew.mcnaughton@pnnl.gov>
2023-10-16 14:18:23 -0700

    Fixing Pains Filter

--- src/cactus/tools/Pains_Filter.py
+++ src/cactus/tools/Pains_Filter.py
@@ -1,24 +1,33 @@
+from langchain.tools import BaseTool
 from rdkit import Chem
 from adme_pred import ADME
 
 
-def pains_filter(self, compound_smiles: str) -> bool:
-    """
-    Uses the adme-pred-py implementation: https://github.com/ikmckenz/adme-pred-py/tree/master
-    From the adme-pred-py github:
-    Baell and Holloway (2010) New Substructure Filters for Removal of Pan
-    Assay Interference Compounds (PAINS) from Screening Libraries and for
-    Their Exclusion in Bioassays
+class pains_filter(BaseTool):
+    name = "pains_filter"
+    description = "returns a boolean value of whether the molecule triggers the Pains Filter"
 
-    This filter finds promiscuous compounds that are likely to show activity
-    regardless of the target.
+    def pains_filter(self, compound_smiles: str) -> bool:
+        """
+        Uses the adme-pred-py implementation: https://github.com/ikmckenz/adme-pred-py/tree/master
+        From the adme-pred-py github:
+        Baell and Holloway (2010) New Substructure Filters for Removal of Pan
+        Assay Interference Compounds (PAINS) from Screening Libraries and for
+        Their Exclusion in Bioassays
 
-    Returns:
-        Boolean of whether the molecule triggers the PAINS filter.
-    """
-    mol = Chem.MolFromSmiles(compound_smiles)
-    mol = ADME(mol)
-    return mol.pains()
+        This filter finds promiscuous compounds that are likely to show activity
+        regardless of the target.
+
+        Returns:
+            Boolean of whether the molecule triggers the PAINS filter.
+        """
+        mol = Chem.MolFromSmiles(compound_smiles)
+        mol = ADME(mol)
+        return mol.pains()
+
+    async def _arun(self, compound_smiles: str) -> bool:
+        """Use the brenk_filter tool asynchronously."""
+        raise NotImplementedError()
 
 
 # example use
